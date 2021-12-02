8b788c98e365a48a0a547361771da422a9694ac9
motazabdou <motazabdou30@gmail.com>
2021-12-02 14:10:12 +0000

    Added add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
     """ View that renders the shopping bag contents page """
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add the number of rental weeks of the specified watch to the shopping bag """
+
+    num_of_weeks = int(request.POST.get('num_of_weeks'))
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += num_of_weeks
+    else:
+        bag[item_id] = num_of_weeks
+
+    request.session['bag'] = bag
+    return redirect(redirect_url)
