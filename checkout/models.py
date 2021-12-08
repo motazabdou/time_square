f994965a93e3063f1754de4f54a0658d92d711dd
motazabdou <motazabdou30@gmail.com>
2021-12-08 18:49:55 +0000

    Added save method to OrderLineItem class

--- checkout/models.py
+++ checkout/models.py
@@ -4,7 +4,7 @@ from django.db.models import Sum
 from django.conf import settings
 from products.models import Product
 
-class order(models.Model):
+class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
@@ -51,8 +51,19 @@ class order(models.Model):
 
 
 
-class orderLineItem(models.Model):
+class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
     num_of_weeks = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the line item total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.num_of_weeks
+        super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
