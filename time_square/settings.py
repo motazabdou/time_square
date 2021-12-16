bc0bc5e3b57a576c341d24a3bfb5d7a0f7a5ef52
motazabdou <motazabdou30@gmail.com>
2021-12-16 19:14:57 +0000

    connected to postgres database

--- time_square/settings.py
+++ time_square/settings.py
@@ -118,17 +118,14 @@ WSGI_APPLICATION = 'time_square.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
 }
+
 
 
 # Password validation
