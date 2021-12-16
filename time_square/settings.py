affacb22d3b74bb14e21c6374827c5db6db15ccd
motazabdou <motazabdou30@gmail.com>
2021-12-16 19:38:06 +0000

    Removed secret key and set debug

--- time_square/settings.py
+++ time_square/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!x)i(ho6vgn!1$t$ag_go(jnh(r!v71s67kl0xfm)q34or^^2)'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['time-square.herokuapp.com', 'localhost']
 
