977e63bd7034faed0926bf64faedb94e8e859276
motazabdou <motazabdou30@gmail.com>
2021-12-17 10:24:19 +0000

    Added if else statement in settings.py to ensure Debug is false if not in development environment

--- time_square/settings.py
+++ time_square/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+if 'DEVELOPMENT' in os.environ:
+    DEBUG = True
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = ['time-square.herokuapp.com', 'localhost']
 
